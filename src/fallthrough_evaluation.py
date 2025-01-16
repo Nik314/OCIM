4dd0bb35146a4bd6bf5bc90b790e3abcc5b30855
Nik314 <niklasvandetten@arcor.de>
2025-01-16 17:38:26 +0100

    Update fallthrough_evaluation.py

---
+++ src/fallthrough_evaluation.py
@@ -0,0 +1,95 @@
+from auxillary_methods import *
+
+
+
+def evaluate_concurrent_fallthrough(part_one, part_two, dfgs, clos, rel, div):
+
+    precision_violation = 0
+    precision_correct = 0
+
+    for a in part_one:
+        for b in part_two:
+            for ot in rel[a] & rel[b]:
+                if not dfgs[ot][0].get((a,b),0):
+                    precision_violation += 1
+                else:
+                    precision_correct += 1
+                if not dfgs[ot][0].get((b,a),0):
+                    precision_violation += 1
+                else:
+                    precision_correct += 1
+
+    return 1- (precision_violation / (precision_correct + precision_violation))
+
+
+def evaluate_xor_fallthrough(part_one, part_two, dfgs, clos, rel, div):
+
+    precision_violation = 0
+    precision_correct = 0
+
+    for a in part_one:
+        for b in part_two:
+            for ot in get_divergent_types(a,b,part_one+part_two,div,rel):
+                if not dfgs[ot][0].get((a, b), 0):
+                    precision_violation += 1
+                else:
+                    precision_correct += 1
+                if not dfgs[ot][0].get((b, a), 0):
+                    precision_violation += 1
+                else:
+                    precision_correct += 1
+
+    return 1 - (precision_violation / (precision_correct + precision_violation))
+
+
+def evaluate_sequence_fallthrough(part_one, part_two, dfgs, clos, rel, div):
+
+    precision_violation = 0
+    precision_correct = 0
+
+    for a in part_one:
+        for b in part_two:
+            for ot in get_divergent_types(a,b,part_one+part_two,div,rel):
+                if not dfgs[ot][0].get((a, b), 0):
+                    precision_violation += 1
+                else:
+                    precision_correct += 1
+                if not dfgs[ot][0].get((b, a), 0):
+                    precision_violation += 1
+                else:
+                    precision_correct += 1
+            for ot in get_non_divergent_types(a,b,part_one+part_two,div,rel):
+                if not clos[ot][0].get((a, b), 0):
+                    precision_violation += 1
+                else:
+                    precision_correct += 1
+
+    return 1 - (precision_violation / (precision_correct + precision_violation))
+
+
+def evaluate_loop_fallthrough(part_one, part_two, dfgs, clos, rel, div):
+
+    precision_violation = 0
+    precision_correct = 0
+
+    for a in part_one:
+        for b in part_two:
+            for ot in get_divergent_types(a,b,part_one+part_two,div,rel):
+                if not dfgs[ot][0].get((a, b), 0):
+                    precision_violation += 1
+                else:
+                    precision_correct += 1
+                if not dfgs[ot][0].get((b, a), 0):
+                    precision_violation += 1
+                else:
+                    precision_correct += 1
+    for a in part_one +part_two:
+        for b in part_one + part_two:
+            for ot in rel[a] & rel[b]:
+                if not clos[ot][0].get((a, b), 0):
+                    precision_violation += 1
+                else:
+                    precision_correct += 1
+
+    return 1 - (precision_violation / (precision_correct + precision_violation))
+
