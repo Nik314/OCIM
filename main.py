26e721ac70cbeb572976cbd4cd33a675b7943af5
Nik314 <niklasvandetten@arcor.de>
2025-04-03 13:48:43 +0200

    update

--- main.py
+++ main.py
@@ -16,9 +16,13 @@ if __name__ == "__main__":
 	#Next, decide which experiments you want to run
 	#By default, all four from the paper are executed
 	#Note, this will take some time and use lots of cores
-	evaluation_util.experiment_1_and_2(data_directory,apply,result_directory)
-	evaluation_util.print_experiment_1(result_directory)
-	evaluation_util.plot_experiment_2(result_directory)
+
+
+	#evaluation_util.experiment_1_and_2(data_directory,apply,result_directory)
+	#evaluation_util.print_experiment_1(result_directory)
+	#evaluation_util.plot_experiment_2(result_directory)
+
+
 	evaluation_util.run_experiment_3("data","results", apply)
 
 
